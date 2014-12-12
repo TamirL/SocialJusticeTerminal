1aa024a5c97c96c66b3dc1255818a97e485406d4
Tamir Lavi <tamir168@gmail.com>
2014-12-12 15:43:54 +0200

    The remains of the previous commit

---
+++ SocialJusticTerminal/SocialJusticTerminal/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.AccessControl;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SocialJusticTerminal.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
